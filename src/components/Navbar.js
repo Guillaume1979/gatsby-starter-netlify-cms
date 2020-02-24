52d0ac2c2cdc66f4dd1667ba225426a929f76b8c
Guillaume1979 <guibetguillaume@gmail.com>
2020-02-24 21:45:57 +0100

    test modif logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,8 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <h1>VERNON Ville Citoyenne</h1>
+              {/*<img src={logo} alt="Kaldi" style={{ width: '88px' }} />*/}
             </Link>
             {/* Hamburger menu */}
             <div
